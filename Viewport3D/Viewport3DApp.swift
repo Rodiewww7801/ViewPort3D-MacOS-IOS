e98432daedcef2e4e5f08db046c627ce8f99b8f9
Rodion Hladchenko <r.hladchenko@pnn.in.ua>
2023-04-09 16:59:12 +0300

    Initial Commit

---
+++ Viewport3D/Viewport3DApp.swift
@@ -0,0 +1,17 @@
+//
+//  Viewport3DApp.swift
+//  Viewport3D
+//
+//  Created by Rodion Hladchenko on 09.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct Viewport3DApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
